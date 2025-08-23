9b36238d80d5e1bf702e6ece62cd782fe368e537
Aditya <aditya7536051@gmail.com>
2025-08-23 16:45:07 +0530

    Update cash.c
    added some comments

--- Week-1/cash.c
+++ Week-1/cash.c
@@ -1,22 +1,26 @@
 // make a program in which usser input 
 // the amount and we return him the minimum 
 // number of coins required as change.
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <ctype.h>      // for isdigit()
+#include <stdio.h>      // for printf(), scanf()
+#include <stdlib.h>     // for atol()
+#include <string.h>     // for strlen()
 
 int coin(int cash);
 int main()
 {
     int cash, coins, valid;
     char check[25];
+    // prompt until user enters a valid cash amount
     do
     {
+        
         printf("Change owed: ");
         scanf("%s", check);
 
         valid = 1;
+        
+        // checking if all character in input are digit
         for (int i = 0; i < (int)strlen(check);i++)
         {   
             if(!isdigit(check[i]))
@@ -27,21 +31,23 @@ int main()
         }
         if (valid)
         {
-            cash = atoi(check);
+            cash = atoi(check);        // convert to integer if input is valid
         }
         else
         {
-            cash = -1;
+            cash = -1;        // Force reprompt on invalid input
         }
         
     }
     while (cash < 0);
+    
     if (cash == 0)
     {
         printf("0\n");
     }
     else
     {
+        // calculating nummber of min coins required
         coins = coin(cash);
         printf("%d\n", coins);
     }
@@ -50,7 +56,10 @@ int main()
 int coin(int cash)
 {
     int count = 0;
+    // making an array of coin denomination in cents
     int coins[] = {25, 10, 5, 1};
+
+    // Loop through each coin denomination, starting from largest
     for (int i = 0; i < 4; i++)
     {
         count = count + cash / coins[i];
