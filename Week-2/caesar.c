3958efbd42d5ceee79a75f782603ea4a05776216
Aditya <aditya7536051@gmail.com>
2025-08-26 13:52:00 +0530

    added comments

--- Week-2/caesar.c
+++ Week-2/caesar.c
@@ -1,18 +1,24 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
+/* a program to take a command line argument (key), and input 
+from the user and convert it into cipher message
+*/ 
+#include <stdio.h>      // printf(), fgets()
+#include <ctype.h>      // isalpha(), isdigit(), isupper()
+#include <stdlib.h>     // atoi()
+#include <string.h>     // strlen()
 
 void print_ciphertext(char *plaintext, int key, int len);
 
+// taking command-line argument
 int main (int argc, char **argv)
 {
+    // checking number of CLI
     if(argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
+    // checking if the input is number or not
     for (int i = 0; i < strlen(argv[1]); i++)
     {
         if(!isdigit(argv[1][i]))
@@ -24,15 +30,17 @@ int main (int argc, char **argv)
 
     int key = atoi(argv[1]);
     
+    // getting text from user
     char plaintext[50];
     printf("plaintext: ");
     fgets(plaintext, sizeof(plaintext), stdin);
     plaintext[strcspn(plaintext, "\n")] = '\0';
 
-
+    
     printf("ciphertext: ");
     int len = strlen(plaintext);
 
+    // printing ciphertext
     print_ciphertext(plaintext, key, len);
     return 0;
 }
@@ -45,15 +53,18 @@ void print_ciphertext(char *plaintext, int key, int len)
 
         if(isalpha(c))
         {
+            // converting uppercase alphabet to cipher text
             if(isupper(c))
             {
                 printf("%c", (c -'A' + key) % 26 + 'A');
             }
+            // converting lowercase alphabet
             else 
             {
                 printf("%c", (c -'a' + key) % 26 + 'a');
             }
         }
+        // if it is a symbol it remains same
         else
         {
             printf("%c", c);
